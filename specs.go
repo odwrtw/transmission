2ca53cc8bb31f27922eb602ea2cd7c8d2a884d22
Nicolas Duhamel <nicolas@jombi.fr>
2015-05-13 20:37:04 +0200

    Improve response support

--- specs.go
+++ specs.go
@@ -71,30 +71,107 @@ var torrentGetFields = []string{
 	"webseedsSendingToUs",
 }
 
+type File struct {
+	BytesCompleted int
+	Length         int
+	Name           string
+}
+
+type FileStats struct {
+	BytesCompleted int
+	Wanted         bool
+	Priority       int
+}
+
+type Peers struct {
+	Address            string
+	ClientName         string
+	ClientIsChoked     bool
+	ClientIsInterested bool
+	FlagStr            string
+	IsDownloadingFrom  bool
+	IsEncrypted        bool
+	IsIncoming         bool
+	IsUploadingTo      bool
+	IsUTP              bool
+	PeerIsChoked       bool
+	PeerIsInterested   bool
+	Port               int
+	Progress           float64
+	RateToClient       int
+	RateToPeer         int
+}
+
+type PeersFrom struct {
+	FromCache    int
+	FromDht      int
+	FromIncoming int
+	FromLpd      int
+	FromLtep     int
+	FromPex      int
+	FromTracker  int
+}
+
+type Trackers struct {
+	Announce string
+	Id       int
+	Scrape   string
+	Tier     int
+}
+
+// type TrackerStats struct {
+// 	Announce              string
+// 	AnnounceState         int
+// 	DownloadCount         int
+// 	HasAnnounced          bool
+// 	HasScraped            bool
+// 	Host                  string
+// 	Id                    int
+// 	IsBackup              bool
+// 	LastAnnouncePeerCount int
+// 	LastAnnounceResult    string
+// 	LastAnnounceStartTime int
+// 	LastAnnounceSucceeded bool
+// 	LastAnnounceTime      int
+// 	LastAnnounceTimedOut  bool
+// 	LastScrapeResult      string
+// 	LastScrapeStartTime   int
+// 	LastScrapeSucceeded   bool
+// 	LastScrapeTime        int
+// 	LastScrapeTimedOut    bool
+// 	LeecherCount          int
+// 	NextAnnounceTime      int
+// 	NextScrapeTim         int
+// 	Scrap                 string
+// 	ScrapeState           int
+// 	SeederCount           int
+// 	Tier                  int
+// }
+
 type Torrents struct {
 	Torrents *[]Torrent `json:"torrents"`
 }
 
 type Torrent struct {
-	ActivityDate      int
-	AddedDate         int
-	BandwidthPriority int
-	Comment           string
-	CorruptEver       int
-	Creator           string
-	DateCreated       int
-	DesiredAvailable  int
-	DoneDate          int
-	DownloadDir       string
-	DownloadedEver    int
-	DownloadLimit     int
-	DownloadLimited   bool
-	Error             int
-	ErrorString       string
-	Eta               int
-	EtaIdle           int
-	// files                        array
-	// fileStats                    array
+	ActivityDate            int
+	AddedDate               int
+	BandwidthPriority       int
+	Comment                 string
+	CorruptEver             int
+	Creator                 string
+	DateCreated             int
+	DesiredAvailable        int
+	DoneDate                int
+	DownloadDir             string
+	DownloadedEver          int
+	DownloadLimit           int
+	DownloadLimited         bool
+	Error                   int
+	ErrorString             string
+	Eta                     int
+	EtaIdle                 int
+	Files                   *[]File
+	FileStats               *[]FileStats
 	HashString              string
 	HaveUnchecked           int
 	HaveValid               int
@@ -110,16 +187,16 @@ type Torrent struct {
 	MetadataPercentComplete float64
 	Name                    string
 	Peerlimit               int
-	// peers                        array
-	PeersConnected int
-	// peersFrom                    object
-	PeersGettingFromUs int
-	PeersSendingToUs   int
-	PercentDone        float64
-	Pieces             string
-	PieceCount         int
-	PieceSize          int
-	// priorities                   array
+	Peers                   *[]Peers
+	PeersConnected          int
+	PeersFrom               PeersFrom
+	PeersGettingFromUs      int
+	PeersSendingToUs        int
+	PercentDone             float64
+	Pieces                  string
+	PieceCount              int
+	PieceSize               int
+	// Priorities         *[]Priorities
 	QueuePosition      int
 	RateDownload       int
 	RateUpload         int
@@ -133,8 +210,8 @@ type Torrent struct {
 	SizeWhenDone       int
 	StartDate          int
 	Status             int
-	// trackers                     array
-	// trackerStats                 array
+	Trackers           *[]Trackers
+	// TrackerStats  *[]TrackerStats
 	TotalSize     int
 	TorrentFile   string
 	UploadedEver  int
@@ -142,6 +219,6 @@ type Torrent struct {
 	UploadLimited bool
 	UploadRatio   float64
 	// wanted                       array
-	// webseeds                     array
+	webseeds            interface{}
 	WebseedsSendingToUs int
 }
